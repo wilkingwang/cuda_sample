25af45717f3fd1f4da7edfb3e967fdd428316038
wilkingwang <wilkingwang@163.com>
2024-12-02 17:19:32 +0800

    基础框架

---
+++ src/Framework/KernelHelper.cu
@@ -0,0 +1,16 @@
+#include "KernelHelper.cuh"
+
+void allocateArray(void** devPtr, size_t size)
+{
+
+}
+
+void copyArrayToDevice(void* device, const void* host, int offset, int size)
+{
+
+}
+
+void copyArrayFromDevice(void* host, const void* device, int size)
+{
+
+}
---
+++ src/Framework/KernelHelper.cuh
@@ -0,0 +1,10 @@
+#pragma once
+
+extern "C"
+{
+	void allocateArray(void** devPtr, size_t size);
+
+	void copyArrayToDevice(void* device, const void* host, int offset, int size);
+
+	void copyArrayFromDevice(void* host, const void* device, int size);
+}
